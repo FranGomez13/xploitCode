df85529bdf815b7dd81532ddcbdae68dcbf99b2a
Fran Gomez <frangomez709@gmail.com>
2019-05-04 20:38:42 -0600

    social login

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
 	facebook:{
 		"clientID": "349764795658829",
 		"clientSecret": "e8a17b73995b2153fad5c121cacb2226",
-		"callbackURL": "http://localhost:3000/auth/facebook/callback"
+		"callbackURL": "http://xploitcode.herokuapp.com/auth/facebook/callback"
 	},
 	github:{
 		"clientID": "01fb9cfd4c2209127a0b",
 		"clientSecret": "7f590c4910010bec6440607638b1d3de7d0f8f83",
-		"callbackURL": "http://localhost:3000/auth/github/callback"
+		"callbackURL": "http://xploitcode.herokuapp.com/auth/github/callback"
 	}
 }
